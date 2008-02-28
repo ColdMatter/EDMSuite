0f029a43bcf03a8cf96528299d0a76c289d20dcd
Chris Sinclair <c.sinclair@ion.ucl.ac.uk>
2008-02-28 15:15:29 +0000

    Commit recent BufferGasHardware.cs

--- DAQ/BufferGasHardware.cs
+++ DAQ/BufferGasHardware.cs
@@ -18,12 +18,22 @@ namespace DAQ.HAL
 
             // map the digital channels
             string pgBoard = (string)Boards["pg"];
-         
-            AddDigitalOutputChannel("valve", pgBoard, 0, 6);
-            AddDigitalOutputChannel("flash", pgBoard, 0, 2);
+
             AddDigitalOutputChannel("q", pgBoard, 0, 0);
+            AddDigitalOutputChannel("aom", pgBoard, 0, 1);//pin 44
+            AddDigitalOutputChannel("flash", pgBoard, 0, 2);
+            //(0,3) pin 12 is unconnected
+            //AddDigitalOutputChannel("shutter", pgBoard, 0, 4);//pin 13
+            //AddDigitalOutputChannel("probe", pgBoard, 0, 1);//pin 13
+            //(0,5) is reserved as the switch line
+            AddDigitalOutputChannel("valve", pgBoard, 0, 6);
+
             AddDigitalOutputChannel("detector", pgBoard, 2, 0); //Pin 16
-            AddDigitalOutputChannel("aom", pgBoard, 0, 1);//
+
+            // add things to the info
+            // the analog triggers
+            Info.Add("analogTrigger0", (string)Boards["daq"] + "/PFI0");
+            Info.Add("analogTrigger1", (string)Boards["daq"] + "/PFI1");
 
             // map the analog channels
             string daqBoard = (string)Boards["daq"];
