cad694757dd8986d99622f231a4fe0feb9cb25a2
Michael Tarbutt <m.tarbutt@imperial.ac.uk>
2012-02-01 18:51:39 +0000

    Mod to RainbowHardware

--- DAQ/RainbowHardware.cs
+++ DAQ/RainbowHardware.cs
@@ -18,8 +18,10 @@ namespace DAQ.HAL
         {
 
             // add the boards
-            Boards.Add("daq", "/PXI1Slot6");
+            Boards.Add("daq", "/PXI1Slot4");
+            Boards.Add("TCLBoard", "/PXI1Slot6");
             string daqBoard = (string)Boards["daq"];
+            string TCLBoard = (string)Boards["TCLBoard"];
 
             // add things to the info
             // the analog triggers
@@ -32,6 +34,15 @@ namespace DAQ.HAL
             Info.Add("PatternGeneratorBoard", daqBoard);
             Info.Add("PGType", "integrated");
             Info.Add("PGClockCounter", "/ctr0");
+
+            //TCL Lockable lasers
+            Info.Add("TCLLockableLasers", new string[] { "laser"});
+            Info.Add("TCLPhotodiodes", new string[] { "cavity", "master", "p1" });// THE FIRST TWO MUST BE CAVITY AND MASTER PHOTODIODE!!!!
+            // Some matching up for TCL
+            Info.Add("laser", "p1");
+           // Info.Add("laser2", "p2");
+           // Info.Add("laser3", "p3");
+            Info.Add("TCLTrigger", TCLBoard + "/PFI0");
 
             // YAG laser
             yag = new BrilliantLaser("ASRL3::INSTR");
@@ -50,10 +61,26 @@ namespace DAQ.HAL
             AddAnalogInputChannel("pmt", daqBoard + "/ai0", AITerminalConfiguration.Nrse);
             AddAnalogInputChannel("norm", daqBoard + "/ai1", AITerminalConfiguration.Nrse);
             AddAnalogInputChannel("iodine", daqBoard + "/ai2", AITerminalConfiguration.Nrse);
-            AddAnalogInputChannel("cavity", daqBoard + "/ai3", AITerminalConfiguration.Nrse);
+            //AddAnalogInputChannel("cavity", daqBoard + "/ai3", AITerminalConfiguration.Nrse);
 
             // map the analog output channels
             AddAnalogOutputChannel("laser", daqBoard + "/ao0");
+            AddAnalogOutputChannel("laser2", daqBoard + "/ao2");
+            AddAnalogOutputChannel("laser3", daqBoard + "/ao3");
+            AddAnalogInputChannel("master", TCLBoard + "/ai1", AITerminalConfiguration.Rse);
+            AddAnalogInputChannel("p1", TCLBoard + "/ai3", AITerminalConfiguration.Rse);
+            AddAnalogInputChannel("cavity", TCLBoard + "/ai2", AITerminalConfiguration.Rse);
+            //AddAnalogInputChannel("p2", TCLBoard + "/ai0", AITerminalConfiguration.Rse);
+            //AddAnalogInputChannel("p3", TCLBoard + "/ai5", AITerminalConfiguration.Rse);
+
+            //Transfer Cavity Lock
+            //AddAnalogOutputChannel("cavity", daqBoard + "/ao1");
+            //Info.Add("analogTrigger2", (string)Boards["daq"] + "/PFI0");
+            //Info.Add("analogTrigger3", (string)Boards["daq"] + "/PFI3");
+            //AddDigitalOutputChannel("scanTrigger", daqBoard, 0, 6);
+            //AddAnalogInputChannel("slavepd", daqBoard + "/ai4", AITerminalConfiguration.Nrse);
+            //AddAnalogInputChannel("masterpd", daqBoard + "/ai5", AITerminalConfiguration.Nrse);
+
 
              // map the counter channels
             //AddCounterChannel("phaseLockOscillator", daqBoard + "/ctr7");
