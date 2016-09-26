746e1c85e3ba356f5bca129c13a0388543b2bb6b
jstammers <jimmy.stammers@gmail.com>
2016-09-26 11:24:15 +0100

    Started ImageViewer and commiting before merging with kyle's branch

---
+++ NavigatorHardwareControl/ImageViewer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using IMAQ;
+
+namespace NavigatorHardwareControl
+{
+    /// <summary>
+    /// Interaction logic for ImageViewer.xaml
+    /// </summary>
+    public partial class ImageViewer : Window
+    {
+        public ImageViewer()
+        {
+            InitializeComponent();
+            ImageViewerWindow imageWindow = new ImageViewerWindow();
+            imageWindow.TopLevel = false;
+            imageWindow.FormBorderStyle = FormBorderStyle.None;
+            wfHost.Child = imageWindow;
+        }
+
+    }
+}
