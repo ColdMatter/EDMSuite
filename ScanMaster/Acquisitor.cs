6708c05d7449a3e522776922551aa706505e46ee
jadevlin <j.devlin11@imperial.ac.uk>
2016-12-10 18:23:12 +0000

    Implement ? to question the value of param in SC

--- ScanMaster/Acquisitor.cs
+++ ScanMaster/Acquisitor.cs
@@ -108,6 +108,7 @@ namespace ScanMaster.Acquire
 
                         ScanPoint sp = new ScanPoint();
 						sp.ScanParameter = config.outputPlugin.ScanParameter;
+                        
 
                         for (int shotNum = 0; shotNum < (int)(config.outputPlugin.Settings["shotsPerPoint"]); shotNum++)
                         {
