1abc301f14d8393ad96ccf31acca8ca15f31105f
Jimmy Stammers <jimmy.stammers@gmail.com>
2017-04-03 11:36:59 +0100

    Added PropertyGrid for configuring hardware and filesystem

--- MotMaster2/Options.xaml.cs
+++ MotMaster2/Options.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
 
 namespace MotMaster2
 {
@@ -22,6 +23,7 @@ namespace MotMaster2
         public Options()
         {
             InitializeComponent();
+          
         }
     }
 }
