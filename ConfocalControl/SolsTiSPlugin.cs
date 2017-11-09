dc8534960d9e47be99c6b08b693e6772689a3733
SebBoissier <s.boissier14@imperial.ac.uk>
2017-11-09 10:45:23 +0000

    Started working on wavemeterscan

--- ConfocalControl/SolsTiSPlugin.cs
+++ ConfocalControl/SolsTiSPlugin.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
 
+using NationalInstruments;
+using NationalInstruments.DAQmx;
+using DAQ.Environment;
 using DAQ.HAL;
 
 namespace ConfocalControl
@@ -23,12 +27,25 @@ namespace ConfocalControl
             return controllerInstance;
         }
 
+        // Define Opt state
+        private enum ScanState { stopped, running, stopping };
+        private ScanState backendState = ScanState.stopped;
+
         // Settings
         public PluginSettings Settings {get; set;}
 
         // Laser
         private ICEBlocSolsTiS solstis;
         public ICEBlocSolsTiS Solstis { get { return solstis; } }
+
+        // Keep track of tasks
+        private List<Point> pointHistory;
+        private Task triggerTask;
+        private DigitalSingleChannelWriter triggerWriter;
+        private Task freqOutTask;
+        private string counterChannel;
+        private Task counterTask;
+        private CounterSingleChannelReader counterReader;
 
         #endregion
 
@@ -48,9 +65,26 @@ namespace ConfocalControl
             if (Settings.Keys.Count != 1)
             {
                 Settings["wavelength"] = 785.0;
+
+                Settings["wavemeterScanStart"] = 780.0;
+                Settings["wavemeterScanStop"] = 790.0;
+                Settings["wavemeterScanPoints"] = 100;
                 return;
             }
+
+            triggerTask = null;
+            freqOutTask = null;
+            counterTask = null;
+
+            triggerWriter = null;
+            counterReader = null;
         }
+
+        #endregion
+
+        #region Wavemeter Scan
+
+
 
         #endregion
 
