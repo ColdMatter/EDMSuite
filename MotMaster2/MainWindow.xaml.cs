c1dac3c29ddc2ac13a257023d915b8864c4cd6eb
teodor-krastev <info@sicyon.com>
2017-11-28 17:34:14 +0000

    changes 27/11a

--- MotMaster2/MainWindow.xaml.cs
+++ MotMaster2/MainWindow.xaml.cs
@@ -1258,7 +1258,7 @@ namespace MOTMaster2
                 controller.WaitForRunToFinish();
                 controller.IncrementBatchNumber();
                 //TODO Make a more descriptive dynamic filename
-                if (cbSaveAfterLoop.IsChecked.Value && mms[multiCount - 1].Value == mms[multiCount - 1].sTo)
+                if (cbSaveAfterLoop.IsChecked.Value && (mms[multiCount - 1].Value + mms[multiCount - 1].sBy) > mms[multiCount - 1].sTo)
                 {
                     controller.RestartMultiScanLogger("test_" + batchNum.ToString()); batchNum++; 
                     //HARDCODED Sleep to allow for DCS to update !!!!
