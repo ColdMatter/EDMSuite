786533faa5b3fa08d6f905cb739358712ef7aaf7
Luke Caldwell <lukecaldwell02@gmail.com>
2018-01-31 10:47:56 +0000

    Update PHBonesawFileSystem after switching to Box Drive

--- DAQ/PHBonesawFileSystem.cs
+++ DAQ/PHBonesawFileSystem.cs
@@ -6,7 +6,7 @@ namespace DAQ.Environment
     {
         public PHBonesawFileSystem()
         {
-            Paths.Add("MOTMasterDataPath", "C:\\Users\\cafmot\\Box Sync\\CaF MOT\\MOTData\\MOTMasterData\\");
+            Paths.Add("MOTMasterDataPath", "C:\\Users\\cafmot\\Box\\CaF MOT\\MOTData\\MOTMasterData\\");
             Paths.Add("scriptListPath", "C:\\ControlPrograms\\EDMSuite\\MoleculeMOTMasterScripts");
             Paths.Add("daqDLLPath", "C:\\ControlPrograms\\EDMSuite\\DAQ\\bin\\CaF\\daq.dll");
             Paths.Add("MOTMasterExePath", "C:\\ControlPrograms\\EDMSuite\\MOTMaster\\bin\\CaF\\");
