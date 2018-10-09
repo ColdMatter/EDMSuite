3ec0440cc0675b6277143fa29b94b7c4e7712b95
Luke Caldwell <lukecaldwell02@gmail.com>
2018-10-09 12:30:20 +0100

    Changes to MoleculeMOTHardwareController
    - To allow viewining of whole program on remote desktop

--- MoleculeMOTHardwareControl/ControlWindow.Designer.cs
+++ MoleculeMOTHardwareControl/ControlWindow.Designer.cs
@@ -118,7 +118,7 @@ namespace MoleculeMOTHardwareControl
             this.tableLayoutPanel9.ColumnCount = 3;
             this.tableLayoutPanel9.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 4.848485F));
             this.tableLayoutPanel9.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 95.15151F));
-            this.tableLayoutPanel9.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel9.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 42F));
             this.tableLayoutPanel9.Controls.Add(this.messageBoxCollapseExpandButton, 2, 0);
             this.tableLayoutPanel9.Controls.Add(this.label8, 1, 0);
             this.tableLayoutPanel9.Controls.Add(this.messageNumberPanel, 0, 0);
@@ -134,7 +134,7 @@ namespace MoleculeMOTHardwareControl
             // 
             this.messageBoxCollapseExpandButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.messageBoxCollapseExpandButton.BackColor = System.Drawing.Color.Transparent;
-            this.messageBoxCollapseExpandButton.Location = new System.Drawing.Point(658, 5);
+            this.messageBoxCollapseExpandButton.Location = new System.Drawing.Point(656, 5);
             this.messageBoxCollapseExpandButton.Name = "messageBoxCollapseExpandButton";
             this.messageBoxCollapseExpandButton.Size = new System.Drawing.Size(23, 23);
             this.messageBoxCollapseExpandButton.TabIndex = 0;
