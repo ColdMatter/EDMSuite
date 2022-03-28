fe8ef281fe689eab6a26b9b7e58eba06cf5f03bb
arijitphd <arijit.phd@gmail.com>
2022-03-28 10:52:17 +0100

    backup

---
+++ DAQ/AnalogPatternBuilderCopy.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Collections;
+using System.Linq;
+using DAQ.HAL;
+using DAQ.Environment;
+
+namespace DAQ.Analog
+{
+    /// <summary>
+    /// thin wrapper around AnalogPatternBuilderSingleBoard
+    /// </summary>
+
+    [Serializable, DataContract]
+    public class AnalogPatternBuilder
+    {
+        [DataMember]
+        private Dictionary<string, AnalogPatternBuilderSingleBoard> boards = new Dictionary<string, AnalogPatternBuilderSingleBoard>();
+        public Dictionary<string, AnalogPatternBuilderSingleBoard> Boards
+        {
+            get { return boards; }
+            set { boards = value; }
+        }
+
+        public AnalogPatternBuilder()
+        {
+
+        }
+
+        public void AddBoard(string address)
+        {
+            Boards.Add(address, new AnalogPatternBuilderSingleBoard());
+        }
+
+        public AnalogPatternBuilderSingleBoard GetBoard(AnalogOutputChannel channel)
+        {
+            string boardName = channel.Device;
+            if (!Boards.ContainsKey(boardName))
+            {
+                AddBoard(boardName);
+            }
+            return Boards[boardName];
+        }
+
+        public void AddChannel(string channelName)
+        {
+            
+            AnalogOutputChannel channel = (AnalogOutputChannel)Environs.Hardware.AnalogOutputChannel[channelName];
+            GetBoard(channel).AddChannel(channel.BitNumber);
+
+        }
+
+        public void AddAnalogValue(string channelName, int time, double value)
+        {
+            AnalogOutputChannel channel = (AnalogOutputChannel)Environs.Hardware.AnalogOutputChannel[channelName];
+            GetBoard(channel).AddAnalogValue(channel.BitNumber, time, value);
+        }
+
+        //value is the voltage during the pulse
+        //finalValue is the voltage AFTER the pulse.
+        public void AddAnalogPulse(string channelName, int startTime, int duration, double value, double finalValue)
+        {
+            AnalogOutputChannel channel = (AnalogOutputChannel)Environs.Hardware.AnalogOutputChannel[channelName];
+            GetBoard(channel).AddAnalogPulse(channel.BitNumber, startTime, duration, value, finalValue);
+        }
+
+        /** Return the minimum length array of the longest pattern. */
+        public int GetMinimumLength()
+        {
+            AnalogPatternBuilderSingleBoard[] boardsList = Boards.Values.ToArray();
+            int numBoards = boardsList.Count();
+            int[] minLengths = new int[numBoards];
+            for (int i = 0; i < numBoards; i++)
+            {
+                minLengths[i] = boardsList[i].GetMinimumLength();
+            }
+            return minLengths.Max();
+        }
+        
+        public double GetValue(string channelName, int time)
+        {
+            AnalogOutputChannel channel = (AnalogOutputChannel)Environs.Hardware.AnalogOutputChannel[channelName];
+            return GetBoard(channel).GetValue(channel.BitNumber, time);
+        }
+        
+        public void AddLinearRamp(string channelName, int startTime, int steps, double finalValue)
+        {
+            AnalogOutputChannel channel = (AnalogOutputChannel)Environs.Hardware.AnalogOutputChannel[channelName];
+            GetBoard(channel).AddLinearRamp(channel.BitNumber, startTime, steps, finalValue);
+        }
+
+        public void AddPolynomialRamp(string channelName, int startTime, int stopTime,
+            double finalValue, double upperThresholdValue, double lowerThresholdValue,
+            double weight1, double weight2, double weight3, double weight4)
+        {
+            AnalogOutputChannel channel = (AnalogOutputChannel)Environs.Hardware.AnalogOutputChannel[channelName];
+            GetBoard(channel).AddPolynomialRamp(channel.BitNumber, startTime, stopTime,
+                finalValue, upperThresholdValue, lowerThresholdValue,
+                weight1, weight2, weight3, weight4);
+        }
+
+        public double[,] BuildPattern()
+        {
+            Pattern = new double[AnalogPatterns.Count, PatternLength];
+            ICollection<string> keys = AnalogPatterns.Keys; // will it send all the keys?
+            int i = 0;
+            foreach (AnalogPatternBuilderSingleBoard board in Boards.Values)
+            {
+                foreach (string key in keys)
+                {
+                    double[] d = board.buildSinglePattern(key);
+                    for (int j = 0; j < PatternLength; j++)
+                    {
+                        Pattern[i, j] = d[j];
+                    }
+                    i++;
+                }
+            }
+
+            return Pattern;
+        }
+
+        public void SwitchOffAtEndOfPattern(string channelName)
+        {
+            AnalogOutputChannel channel = (AnalogOutputChannel)Environs.Hardware.AnalogOutputChannel[channelName];
+            GetBoard(channel).SwitchOffAtEndOfPattern(channel.BitNumber);
+        }
+        public void SwitchAllOffAtEndOfPattern()
+        {
+            AnalogOutputChannel channel = (AnalogOutputChannel)Environs.Hardware.AnalogOutputChannel[channelName];
+            GetBoard(channel).SwitchAllOffAtEndOfPattern();
+        }
+    }
+}
---
+++ DAQ/AnalogPatternBuilderSingleBoard.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Collections;
+using DAQ.HAL;
+using DAQ.Environment;
+
+namespace DAQ.Analog
+{
+    /// <summary>
+    /// A class for building analog patterns that can be output by a NI PatternList generator.
+    /// This class lets you set a value (AddAnalogValue), do a linear ramp (AddLinearRamp) and have 
+    /// an analog pulse (AddAnalogPulse).
+    /// 
+    /// </summary>
+
+    [Serializable, DataContract]
+    public class AnalogPatternBuilderSingleBoard
+    {
+        [DataMember]
+        public Dictionary<String, Dictionary<Int32, Double>> AnalogPatterns;
+        public int PatternLength;
+        public double[,] Pattern;
+        private static Hashtable calibrations = Environs.Hardware.Calibrations;
+
+        public AnalogPatternBuilderSingleBoard(string[] channelNames, int patternLength)
+        {
+            AnalogPatterns = new Dictionary<string, Dictionary<int, double>>();
+            PatternLength = patternLength;
+            int numberOfChannels = channelNames.GetLength(0);
+            for (int i = 0; i < numberOfChannels; i++)
+            {
+                AddChannel(channelNames[i]);
+            }
+        }
+        public AnalogPatternBuilderSingleBoard(int patternLength)
+        {
+            AnalogPatterns = new Dictionary<string, Dictionary<int, double>>();
+            PatternLength = patternLength;
+        }
+
+        public void AddChannel(string channelName)
+        {
+            Dictionary<int, double> d = new Dictionary<int, double>();
+            AnalogPatterns.Add(channelName, d);
+
+        }
+
+        public void AddAnalogValue(string channel, int time, double value)
+        {
+            if (time < PatternLength)
+            {
+                AnalogPatterns[channel][time] = value;
+            }
+            else
+            {
+                throw new InsufficientPatternLengthException();
+            }
+        }
+
+        //value is the voltage during the pulse
+        //finalValue is the voltage AFTER the pulse.
+        public void AddAnalogPulse(string channel, int startTime, int duration, double value, double finalValue)
+        {
+            if (startTime + duration < PatternLength)
+            {
+                AddAnalogValue(channel, startTime, value);
+                AddAnalogValue(channel, startTime + duration, finalValue);
+            }
+            else
+            {
+                throw new InsufficientPatternLengthException();
+            }
+        }
+
+        private List<int> getSortedListOfEvents(string channel)
+        {
+            List<int> ints = new List<int>(AnalogPatterns[channel].Keys);
+            ints.Sort();
+
+            return ints;
+        }
+        public double GetValue(string channel, int time)
+        {
+            List<int> events = getSortedListOfEvents(channel);
+            double val = 0.0;
+            for (int i = 0; i < AnalogPatterns[channel].Count; i++)
+            {
+                if (events[i] <= time)
+                {
+                    val = AnalogPatterns[channel][events[i]];
+                }
+            }
+            return val;
+        }
+        public void AddLinearRamp(string channel, int startTime, int steps, double finalValue)
+        {
+            if (PatternLength > startTime + steps)
+            {
+                double startValue = GetValue(channel, startTime);
+                double stepSize = (finalValue - startValue) / steps;
+                for (int i = 0; i < steps; i++)
+                {
+                    if (AnalogPatterns[channel].ContainsKey(startTime + i) == false)
+                    {
+                        AddAnalogValue(channel, startTime + i, startValue + (i + 1) * stepSize);
+                    }
+                    else
+                    {
+                        throw new ConflictInPatternException();
+                    }
+                }
+            }
+            else
+            {
+                throw new InsufficientPatternLengthException();
+            }
+        }
+        public void AddPolynomialRamp(string channel, int startTime, int stopTime,
+            double finalValue, double upperThresholdValue, double lowerThresholdValue,
+            double weight1, double weight2, double weight3, double weight4)
+        {
+            if (PatternLength > stopTime)
+            {
+                double startValue = GetValue(channel, startTime);
+                int steps = stopTime - startTime;
+                for (int i = 0; i < steps; i++)
+                {
+                    if (AnalogPatterns[channel].ContainsKey(startTime + i) == false)
+                    {
+                        double t = 1.0 * steps;
+                        double it = 1.0 * i;
+                        double first = weight1 * (it / t);
+                        double second = weight2 * (it / t) * (it / t);
+                        double third = weight3 * (it / t) * (it / t) * (it / t);
+                        double fourth = weight4 * (it / t) * (it / t) * (it / t) * (it / t);
+                        double norm = startValue + weight1 + weight2 + weight3 + weight4;
+                        double value = finalValue * (startValue + first + second + third + fourth) / norm;
+                        if (value > upperThresholdValue)
+                        {
+                            AddAnalogValue(channel, startTime + i, upperThresholdValue);
+                        }
+                        else if (value < lowerThresholdValue)
+                        {
+                            AddAnalogValue(channel, startTime + i, lowerThresholdValue);
+                        }
+                        else
+                        {
+                            AddAnalogValue(channel, startTime + i, value);
+                        }
+                    }
+                    else
+                    {
+                        throw new ConflictInPatternException();
+                    }
+                }
+            }
+            else
+            {
+                throw new InsufficientPatternLengthException();
+            }
+        }
+
+        //For a single channel, gets a sequence of events (changes to the output value) and builds a pattern.
+        public double[] buildSinglePattern(string channel)
+        {
+            double[] d = new double[PatternLength];
+            List<int> events = getSortedListOfEvents(channel);
+            int timeUntilNextEvent = 0;
+            events.Add(PatternLength);
+            if (calibrations.ContainsKey(channel))
+            {
+                for (int i = 0; i < events.Count - 1; i++)
+                {
+                    timeUntilNextEvent = events[i + 1] - events[i];
+                    double dval = AnalogPatterns[channel][events[i]];
+                    for (int j = 0; j < timeUntilNextEvent; j++)
+                    {
+                        d[events[i] + j] = ((Calibration)calibrations[channel]).Convert(dval);
+                    }
+
+                }
+            }
+            else
+            {
+                for (int i = 0; i < events.Count - 1; i++)
+                {
+                    timeUntilNextEvent = events[i + 1] - events[i];
+                    double dval = AnalogPatterns[channel][events[i]];
+                    for (int j = 0; j < timeUntilNextEvent; j++)
+                    {
+                        d[events[i] + j] = dval;
+                    }
+
+                }
+            }
+            
+
+            return d;
+        }
+
+
+        public double[,] BuildPattern()
+        {
+            Pattern = new double[AnalogPatterns.Count, PatternLength];
+            ICollection<string> keys = AnalogPatterns.Keys;
+            int i = 0;
+            foreach (string key in keys)
+            {
+                double[] d = buildSinglePattern(key);
+                for (int j = 0; j < PatternLength; j++)
+                {
+                    Pattern[i, j] = d[j];
+                }
+                i++;
+            }
+
+            return Pattern;
+        }
+
+        public void SwitchOffAtEndOfPattern(string channel)
+        {
+            AddAnalogValue(channel, PatternLength - 1, 0.0);
+        }
+        public void SwitchAllOffAtEndOfPattern()
+        {
+            ICollection<string> keys = AnalogPatterns.Keys;
+            foreach (string key in keys)
+            {
+                AddAnalogValue(key, PatternLength - 1, 0.0);
+            }
+        }
+
+        public class ConflictInPatternException : ApplicationException { }
+        public class InsufficientPatternLengthException : ApplicationException { }
+        public class PatternBuildException : ApplicationException
+        {
+            public PatternBuildException(String message) : base(message) { }
+        }
+    }
+}
