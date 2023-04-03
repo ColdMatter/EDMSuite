7bc3326328131a932ca8a5ffd836893a31d1fd69
lyuqinshu <lyuqinshu@outlook.com>
2023-04-03 11:19:02 +0100

    Added TweezerMOTMasterScripts

---
+++ TweezerMOTMasterScripts/AMOTBasic.cs
@@ -0,0 +1,100 @@
+﻿using MOTMaster;
+using MOTMaster.SnippetLibrary;
+
+using System;
+using System.Collections.Generic;
+
+using DAQ.Pattern;
+using DAQ.Analog;
+
+// This script is supposed to be the basic script for loading a molecule MOT.
+// Note that times are all in units of the clock periods of the two pattern generator boards (at present, both are 10us).
+// All times are relative to the Q switch, though note that this is not the first event in the pattern.
+public class Patterns : MOTMasterScript
+{
+    public Patterns()
+    {
+        Parameters = new Dictionary<string, object>();
+        Parameters["PatternLength"] = 50000;
+        Parameters["TCLBlockStart"] = 4000; // This is a time before the Q switch
+        Parameters["TCLBlockDuration"] = 15000;
+
+        Parameters["TriggerStart"] = 1000;
+        Parameters["TriggerDuration"] = 1000;
+        Parameters["testValue1"] = 0.5;
+        Parameters["testValue2"] = 1.7;
+
+    }
+
+    public override PatternBuilder32 GetDigitalPattern()
+    {
+        PatternBuilder32 p = new PatternBuilder32();
+        int patternStartBeforeQ = (int)Parameters["TCLBlockStart"];
+
+        p.Pulse(patternStartBeforeQ, 0, 10, "aoTrigger");
+        p.Pulse(patternStartBeforeQ, (int)Parameters["TriggerStart"], (int)Parameters["TriggerDuration"], "cameraTrigger");
+        
+        return p;
+    }
+
+    public override AnalogPatternBuilder GetAnalogPattern()
+    {
+        AnalogPatternBuilder p = new AnalogPatternBuilder((int)Parameters["PatternLength"]);
+
+        p.AddChannel("steppingBBias");
+
+        // Add Analog Channels
+
+        p.AddChannel("v00Intensity");
+        p.AddChannel("v00Frequency");
+        p.AddChannel("xShimCoilCurrent");
+        p.AddChannel("yShimCoilCurrent");
+        p.AddChannel("zShimCoilCurrent");
+        p.AddChannel("v00EOMAmp");
+        p.AddChannel("v00Chirp");
+        p.AddChannel("lightSwitch");
+
+        p.AddAnalogValue("lightSwitch", 0, 0.0);
+        //p.AddAnalogValue("lightSwitch", 1000, 2.0);
+
+        // Slowing field
+        p.AddAnalogValue("slowingCoilsCurrent", 0, (double)Parameters["slowingCoilsValue"]);
+        p.AddAnalogValue("slowingCoilsCurrent", (int)Parameters["slowingCoilsOffTime"], 0.0);
+
+
+
+        // B Field
+        p.AddAnalogValue("MOTCoilsCurrent", 0, (double)Parameters["MOTCoilsCurrentValue"]);
+        p.AddAnalogValue("MOTCoilsCurrent", (int)Parameters["MOTCoilsSwitchOff"], 0.0);
+
+
+        // Shim Fields
+        p.AddAnalogValue("xShimCoilCurrent", 0, (double)Parameters["xShimLoadCurrent"]);
+        p.AddAnalogValue("yShimCoilCurrent", 0, (double)Parameters["yShimLoadCurrent"]);
+        p.AddAnalogValue("zShimCoilCurrent", 0, (double)Parameters["zShimLoadCurrent"]);
+
+        // trigger delay
+        // p.AddAnalogValue("triggerDelay", 0, (double)Parameters["triggerDelay"]);
+
+        // F=0
+        //p.AddAnalogValue("v00EOMAmp", 0, 4.4); // 4.4
+        p.AddAnalogValue("v00EOMAmp", 0, 4.9); //24/03/2023
+
+        // v0 Intensity Ramp
+        p.AddAnalogValue("v00Intensity", 0, (double)Parameters["v0IntensityRampStartValue"]);
+
+        // v0 Frequency Ramp
+        p.AddAnalogValue("v00Frequency", 0, (double)Parameters["v0FrequencyStartValue"]);
+        //p.AddAnalogValue("v00Frequency", 4000, 9.0);
+
+        //v0 chirp
+        p.AddAnalogValue("v00Chirp", 0, 0.0);
+
+        //p.AddAnalogValue("transferCoils", 0, 1.0);
+
+        //p.AddAnalogValue("MOTCoilsCurrent", 0, (double)Parameters["Dummy"]);
+
+        return p;
+    }
+
+}
