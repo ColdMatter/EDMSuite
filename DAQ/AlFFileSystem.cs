7ce5e0cca274b3805d3ce6ff08d054fb7122ac81
Lajos Palanki <lp618@ic.ac.uk>
2023-11-28 15:54:52 +0000

    Improvements to AlFHardwareControl and MM for AlF

--- DAQ/AlFFileSystem.cs
+++ DAQ/AlFFileSystem.cs
@@ -12,9 +12,12 @@ namespace DAQ.Environment
 			Paths.Add("MOTMasterDataPath", "C:\\Users\\alfultra\\OneDrive - Imperial College London\\Desktop\\Data\\mot_master_data\\");
 			Paths.Add("scriptListPath", "C:\\EDMSuite\\AlFMOTMasterScripts");
 			Paths.Add("daqDLLPath", "C:\\EDMSuite\\DAQ\\bin\\AlF\\daq.dll");
-			Paths.Add("MOTMasterExePath", "C:EDMSuite\\bin\\AlF\\");
+			Paths.Add("MOTMasterExePath", "C:\\EDMSuite\\bin\\AlF\\");
+			Paths.Add("HardwareClassPath", "C:\\EDMSuite\\DAQ\\AlFHardware.cs");
 			Paths.Add("fakeData", "C:\\Users\\alfultra\\OneDrive - Imperial College London\\Desktop\\Data\\Examples\\");
 			Paths.Add("scanMasterDataPath", "C:\\Users\\alfultra\\OneDrive - Imperial College London\\Desktop\\Data\\");
+			Paths.Add("ToFFilesPath", "C:\\Users\\alfultra\\Documents\\ToF_Data\\");
+			Paths.Add("ExternalFilesPath", "C:\\Users\\alfultra\\Documents\\Camera_images");
 
 			DataSearchPaths.Add(Paths["scanMasterDataPath"]);
 			DataSearchPaths.Add(Paths["fakeData"]);
