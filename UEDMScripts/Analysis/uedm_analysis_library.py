512b81619f24470619822869fcc33791c27c842a
Elise Wursten <e.wursten@imperial.ac.uk>
2026-08-11 14:07:59 +0100

    UEDM analysis: CCD image processing updated

--- UEDMScripts/Analysis/uedm_analysis_library.py
+++ UEDMScripts/Analysis/uedm_analysis_library.py
@@ -375,7 +375,7 @@ def ReadTiff(File):
         images.append(np.array(img))
     return np.array(images)
 
-def ProcessCCDimages(FileCCDA, FileCCDB, FilePMT, CCDAsettings, CCDBsettings, AveragingIntervals):
+def ProcessCCDimagesWithIntervals(FileCCDA, FileCCDB, FilePMT, CCDAsettings, CCDBsettings, AveragingIntervals):
 
     # Read in and reshape the CCD images according to the meta data in the PMT file
     _, _, _, DataOff, Setpoint, _, _ = ProcessAllScansInZippedXML(FilePMT)
@@ -443,7 +443,7 @@ def ProcessCCDimages(FileCCDA, FileCCDB, FilePMT, CCDAsettings, CCDBsettings, Av
     FramesBOn = np.full((NrFramesPerShot,NrIntervals,np.shape(BOn)[2],np.shape(BOn)[3]), np.nan)
     for i in range(NrFramesPerShot):
         for j in range(NrIntervals):
-            Indi = (ShotNr>=AveragingIntervals[j,0]) & (ShotNr< AveragingIntervals[j,1])
+            Indi = (ShotNr>=AveragingIntervals[j][0]) & (ShotNr< AveragingIntervals[j][1])
             FrameA = RemoveCCDoffsetCounts(np.mean(AOn[Indi,i,:,:], axis=0), CCDAsettings)
             FrameB = RemoveCCDoffsetCounts(np.mean(BOn[Indi,i,:,:], axis=0), CCDBsettings)
             FramesAOn[i,j,:,:] = ConvertCountsToPhotons(FrameA, CCDAsettings)
@@ -454,7 +454,7 @@ def ProcessCCDimages(FileCCDA, FileCCDB, FilePMT, CCDAsettings, CCDBsettings, Av
         FramesBOff = np.full((NrFramesPerShot,NrIntervals,np.shape(BOn)[2],np.shape(BOn)[3]), np.nan)
         for i in range(NrFramesPerShot):
             for j in range(NrIntervals):
-                Indi = (ShotNr>=AveragingIntervals[j,0]) & (ShotNr< AveragingIntervals[j,1])
+                Indi = (ShotNr>=AveragingIntervals[j][0]) & (ShotNr< AveragingIntervals[j][1])
                 FrameA = RemoveCCDoffsetCounts(np.mean(AOff[Indi,i,:,:], axis=0), CCDAsettings)
                 FrameB = RemoveCCDoffsetCounts(np.mean(BOff[Indi,i,:,:], axis=0), CCDBsettings)
                 FramesAOff[i,j,:,:] = ConvertCountsToPhotons(FrameA, CCDAsettings)
@@ -465,6 +465,90 @@ def ProcessCCDimages(FileCCDA, FileCCDB, FilePMT, CCDAsettings, CCDBsettings, Av
 
     return AOnPhotons, AOffPhotons, BOnPhotons, BOffPhotons, FramesAOn, FramesAOff, FramesBOn, FramesBOff
 
+def ProcessCCDimages(FileCCDA, FileCCDB, FilePMT, CCDAsettings, CCDBsettings):
+
+    # Read in and reshape the CCD images according to the meta data in the PMT file
+    _, _, _, DataOff, Setpoint, _, _ = ProcessAllScansInZippedXML(FilePMT)
+    Scan = ReadAverageScanInZippedXML(FilePMT)
+    NrPoints = len(Setpoint)
+    ShotNr = range(NrPoints)
+    ThereAreOffShots = not (np.shape(DataOff)[0]==0)
+    if ThereAreOffShots:
+        OnOff = 2
+    else:    
+        OnOff = 1
+    GainA = Scan.GetSetting("shot","ccd1Gain")
+    CCDAsettings['Gain'] = GainA
+    GainB = Scan.GetSetting("shot","ccd2Gain")
+    CCDBsettings['Gain'] = GainB
+
+    # Process CCD A
+    print("Processing CCD A images...")
+    CCDimages = ReadTiff(FileCCDA)
+    NrFramesPerShot = int(np.shape(CCDimages)[0]/NrPoints/OnOff)
+    Shots = np.reshape(CCDimages, (int(NrPoints*OnOff), NrFramesPerShot, np.shape(CCDimages)[1], np.shape(CCDimages)[2]) )
+
+    # For each shot, calculate the integrated counts
+    AOn = Shots[0::OnOff,:,:,:]
+    if ThereAreOffShots:
+        AOff = Shots[1::OnOff,:,:,:]
+    else:
+        AOff = np.array([])
+    
+    AOnPhotons = np.full((NrFramesPerShot,NrPoints), np.nan)
+    AOffPhotons = np.full((NrFramesPerShot,NrPoints), np.nan)
+    for i in range(np.shape(AOn)[1]):
+        for j in range(np.shape(AOn)[0]):
+            AOnPhotons[i,j] = GetCCDphotons(AOn[j,i,:,:], CCDAsettings)
+    if ThereAreOffShots:
+        for i in range(np.shape(AOff)[1]):
+            for j in range(np.shape(AOff)[0]):
+                AOffPhotons[i,j] = GetCCDphotons(AOff[j,i,:,:], CCDAsettings)
+        
+    # Process CCD B
+    print("Processing CCD B images...")
+    CCDimages = ReadTiff(FileCCDB)
+    NrFramesPerShot = int(np.shape(CCDimages)[0]/NrPoints/OnOff)
+    Shots = np.reshape(CCDimages, (int(NrPoints*OnOff), NrFramesPerShot, np.shape(CCDimages)[1], np.shape(CCDimages)[2]) )
+
+    # For each shot, calculate the integrated counts
+    BOn = Shots[0::OnOff,:,:,:]
+    if ThereAreOffShots:
+        BOff = Shots[1::OnOff,:,:,:]
+    else:
+        BOff = np.array([])
+    
+    BOnPhotons = np.full((NrFramesPerShot,NrPoints), np.nan)
+    BOffPhotons = np.full((NrFramesPerShot,NrPoints), np.nan)
+    for i in range(NrFramesPerShot):
+        for j in range(np.shape(BOn)[0]):
+            BOnPhotons[i,j] = GetCCDphotons(BOn[j,i,:,:], CCDBsettings)
+    if ThereAreOffShots:
+        for i in range(NrFramesPerShot):
+            for j in range(np.shape(BOff)[0]):
+                BOffPhotons[i,j] = GetCCDphotons(BOff[j,i,:,:], CCDBsettings)
+    
+    FramesAOn = np.full((NrFramesPerShot,np.shape(AOn)[2],np.shape(AOn)[3]), np.nan)
+    FramesBOn = np.full((NrFramesPerShot,np.shape(BOn)[2],np.shape(BOn)[3]), np.nan)
+    for i in range(NrFramesPerShot):
+        FrameA = RemoveCCDoffsetCounts(np.mean(AOn[:,i,:,:], axis=0), CCDAsettings)
+        FrameB = RemoveCCDoffsetCounts(np.mean(BOn[:,i,:,:], axis=0), CCDBsettings)
+        FramesAOn[i,:,:] = ConvertCountsToPhotons(FrameA, CCDAsettings)
+        FramesBOn[i,:,:] = ConvertCountsToPhotons(FrameB, CCDBsettings)
+
+    if ThereAreOffShots:
+        FramesAOff = np.full((NrFramesPerShot,np.shape(AOn)[2],np.shape(AOn)[3]), np.nan)
+        FramesBOff = np.full((NrFramesPerShot,np.shape(BOn)[2],np.shape(BOn)[3]), np.nan)
+        for i in range(NrFramesPerShot):
+            FrameA = RemoveCCDoffsetCounts(np.mean(AOff[:,i,:,:], axis=0), CCDAsettings)
+            FrameB = RemoveCCDoffsetCounts(np.mean(BOff[:,i,:,:], axis=0), CCDBsettings)
+            FramesAOff[i,:,:] = ConvertCountsToPhotons(FrameA, CCDAsettings)
+            FramesBOff[i,:,:] = ConvertCountsToPhotons(FrameB, CCDBsettings)
+    else:
+        FramesAOff = np.array([])
+        FramesBOff = np.array([])
+
+    return AOnPhotons, AOffPhotons, BOnPhotons, BOffPhotons, FramesAOn, FramesAOff, FramesBOn, FramesBOff
 
 def ProcessCCDimagesWithoutPMT(FileCCDA, FileCCDB, CCDAsettings, CCDBsettings, OnOff, BurstNumber):
 
@@ -993,7 +1077,7 @@ def NoiseEstimatePMT(PhotonF1, PhotonF0, BackgroundF1, BackgroundF0, RatioBinVsB
     return Asy, sAsy
 
 def NoiseEstimateFromAsymmetryPMT(Asymmetry, NphotonTotal, BackgroundF1, BackgroundF0, RatioBinVsBg):
-    Ffactor = 1.5
+    Ffactor = 1.376
     Fcycling = 1.3**2
     PhotonF1 = NphotonTotal * (1 - Asymmetry) / 2
     PhotonF0 = NphotonTotal * (1 + Asymmetry) / 2
